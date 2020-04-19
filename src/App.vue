773f1f19e0e0c6e8f80249286bbd4de14688fbc6
es5es5 <es5e5s@nate.com>
2020-04-19 19:11:10 +0900

    Top init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <Top></Top>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Top from './components/Top'
+
+export default {
+  name: 'App',
+  components: {
+    Top
+  }
+}
+</script>
 
 <style lang="scss">
 @import 'colony-vue/src/assets/scss/reset.scss';
---
+++ src/components/Top.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="top">
+    <div class="top_inner">
+      로그인 | 이용권 | 편성표 | 이벤트 | 다운로드 | 티빙키즈
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
+
+<style lang="scss" scoped>
+#top {
+  padding: .6rem;
+  background-color: #E51D3E;
+  color: lightgray;
+  font-size: .8rem;
+  text-align: right;
+}
+
+.top_inner {
+  max-width: 1024px;
+}
+</style>
